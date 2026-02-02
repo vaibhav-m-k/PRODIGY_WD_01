3a40c751bd9643f6c523c67a087b005b98dd14b4
vaibhav-m-k <vaibhavdaryapur@gmail.com>
2026-02-02 21:20:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-window.addEventListener("scroll", function () {
-    const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+  const navbar = document.getElementById("navbar");
 
-    if (window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled");
-    }
+  if (window.scrollY > 80) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
 });
